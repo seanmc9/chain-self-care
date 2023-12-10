4067158ee4848f3e47dcdc4672ed658081e48924
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-09 23:49:30 -0500

    feat: implement drinkWater

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract DrinkWater {
+    uint256 constant SECONDS_IN_EIGHT_HOURS = 28800;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    address[] public addressesThatHaveDrank;
+    mapping(address => bool) public hasAddressDrank;
+    mapping(address => uint256) public totalNumAddressDrinks;
+    mapping(address => uint256) public lastAddressDrink;
 
-    function increment() public {
-        number++;
+    error CanOnlyDrinkEvery8Hours();
+
+    function drinkWater() public {
+        if (!hasAddressDrank[msg.sender]) {
+            lastAddressDrink[msg.sender] = block.timestamp;
+            totalNumAddressDrinks[msg.sender] = 1;
+            addressesThatHaveDrank.push(msg.sender);
+            hasAddressDrank[msg.sender] = true;
+            return;
+        }
+        
+        uint256 lastDrinkOfThisAddress = lastAddressDrink[msg.sender];
+        if ((block.timestamp - lastDrinkOfThisAddress) < SECONDS_IN_EIGHT_HOURS) {
+            revert CanOnlyDrinkEvery8Hours();
+        } else {
+            lastAddressDrink[msg.sender] = block.timestamp;
+            totalNumAddressDrinks[msg.sender]++;
+        }
     }
 }
